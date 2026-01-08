7a96f93131e65eda0183141c37cf787af40a8e99
jonimax11000 <jonimax11000@gmail.com>
2026-01-08 11:40:26 +0100

    inicio proyecto de springboot, solo holamundo

---
+++ Sprint 3/Services/springboot/src/main/java/com/pi/springboot/controller/videoController.java
@@ -0,0 +1,16 @@
+package com.pi.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class videoController {
+	@GetMapping("/")
+    @ResponseBody
+    public String helloWorld() {
+        return "Pruebas de distincion";
+    }
+}
