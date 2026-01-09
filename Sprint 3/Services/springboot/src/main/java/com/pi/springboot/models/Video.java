d13ead7c085d7373de84223219c52ed1594207d6
jonimax11000 <jonimax11000@gmail.com>
2026-01-09 12:21:02 +0100

    entity Video

---
+++ Sprint 3/Services/springboot/src/main/java/com/pi/springboot/models/Video.java
@@ -0,0 +1,43 @@
+package com.pi.springboot.models;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Table
+@ToString
+public class Video implements Serializable{
+	
+	static final long serialVersionUID = 137L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column
+	private String titol;
+	
+	@Column(name="url")
+	private String videoURL;
+	
+	@Column(name="thumbnail")
+	private String thumbnailURL;
+	
+	@Column
+	private int duracio;
+}
--- Sprint 3/Services/springboot/src/main/java/com/pi/springboot/models/video.java
+++
@@ -1,5 +0,0 @@
-package com.pi.springboot.models;
-
-public class video {
-
-}
