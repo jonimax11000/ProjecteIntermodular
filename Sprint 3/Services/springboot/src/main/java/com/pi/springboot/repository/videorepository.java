76290b7dd12003de3b300e0849d68b46808993ce
Paguita999 <guillemruano06@gmail.com>
2026-01-09 12:44:13 +0100

    Update videorepository.java

--- Sprint 3/Services/springboot/src/main/java/com/pi/springboot/repository/videorepository.java
+++ Sprint 3/Services/springboot/src/main/java/com/pi/springboot/repository/videorepository.java
@@ -1,5 +1,15 @@
 package com.pi.springboot.repository;
 
-public interface videorepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pi.springboot.models.Video;
+
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface videorepository extends JpaRepository<Video, Long> {
 }
